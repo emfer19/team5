d9eccf6be0a479a9fa9cd5ae53232b22aa7e427c
Holden Duncan <holden.duncan@slu.edu>
2019-03-03 20:46:12 -0600

    Work done on the sim

--- org/team5/app/main/ProcessingThread.java
+++ org/team5/app/main/ProcessingThread.java
@@ -3,6 +3,7 @@ package org.team5.app.main;
 import org.team5.app.dataprocessing.DataPoint;
 import org.team5.app.gui.SwingUI;
 
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 
 public class ProcessingThread implements Runnable {
@@ -34,16 +35,49 @@ public class ProcessingThread implements Runnable {
         long sumMessageRates = 0;
         long sleepTime = 150; //in millisecond
         int progressBarUpdater = 0;
-
+        
+        boolean primed = false;
+        double processTime = 2d*(0.000000001d); //Change this to the input from the window
+        DataAnalyzer analyzer = new DataAnalyzer();
+        bufferedMessages = new Queue<int[2]>();
+        
         try {
 
             DataPoint messageRate;
+            SimClock clock = new SimClock(processTime);
             //Note: value of -1 marks the end of the buffer content.
             while ((messageRate = buffer.take()).getValue() != -1) {
 
                 long startTime = System.nanoTime(); //Get nano time just before removing message data from buffer
                 //-------------------------------
                 // Do some processing around at this point.
+                
+                //First startup
+                if(!primed){
+                    clock.setStartTime(messageRate.getTimeIn());
+                    primed = true;
+                }
+                //Updating clock
+                else{
+                    clock.update();
+                    if(clock.isNextSecond()){ //If it's a new minute update the message rate
+                        messageRate = buffer.take()
+                    }
+                    //Adding messages to the buffered data based on the current rate
+                    bufferedMessages.add({messageRate.getValue(), clock.getTime()});
+                }
+                //Updating Data
+                //Update the current set of messages once the program gets through that backlog
+                if(message[1] <= 0){
+                    if(bufferedMessages.peek() != null){message = bufferedMessages.remove();}
+                }
+                else{
+                    message[1]--;
+                }
+                
+                //Recording Stats
+                analyzer.writeData(message[0], clock.getTime());
+                
                 // Let's just simulate work time with Thread.sleep()
                 try {
                     Thread.sleep(sleepTime);
@@ -68,6 +102,7 @@ public class ProcessingThread implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+<<<<<<< HEAD
 
         SwingUI.textArea.append("Total latency (ns): " + sumProcessTime+"\n");
         SwingUI.textArea.append("No of Messages: " + sumMessageRates+"\n");
