020754c005a8f9dbe7911ad896a68086b66d7f2a
Holden Duncan <holden.duncan@slu.edu>
2019-04-23 12:55:35 -0500

    Created Interface

---
+++ org/team5/app/main/IThreadIO.java
@@ -0,0 +1,15 @@
+package org.team5.app.main;
+/*
+* Interface: IThreadIO
+* Holden Duncan
+* An interface for input threads and processing threads
+* to pass messages around. Also to be used by the DataAnalyzer
+* to intake data at the end of the process.
+*/
+
+public interface IThreadIO {
+
+public void add(DataPoint p); //add should take the data and store it or use it
+public DataPont take(); //take should be called to receive the next piece of data from that object
+
+}
